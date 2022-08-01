e0a8b993dbae7067afe86cb9a69f46252e8ad701
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 13:19:20 +0300

    if conditionals
    internship

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 10;
+            var number1 = 15;
+            if(number == 10)
+            {
+                Console.WriteLine("The number is 10!");
+            }
+            else
+            {
+                Console.WriteLine("The number is not 10!");
+            }
+
+
+            Console.WriteLine("Single Line if: ");
+            Console.WriteLine(number == 10 ? "The number is 10!" : "The number is not 10!");
+
+            if(number1 == 15)
+            {
+                Console.WriteLine("The number1 is 15!");
+            }
+            else if(number1 == 25)
+            {
+                Console.WriteLine("The number1 is 25!");
+            }
+            else
+            {
+                Console.WriteLine("The number1 is not 15 or 25!");
+            }
+            
+            Console.ReadLine();
+        }
+
+    }
+}
